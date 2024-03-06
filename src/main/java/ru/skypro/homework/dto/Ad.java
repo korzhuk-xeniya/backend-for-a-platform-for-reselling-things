a9f83116cac89f85399b96f555bb572c32623377
Lebedeva-Elena <lebedeva_leb.en@mail.ru>
2024-03-06 21:00:17 +0300

    добавила модель Объявления

---
+++ src/main/java/ru/skypro/homework/dto/Ad.java
@@ -0,0 +1,13 @@
+package ru.skypro.homework.dto;
+
+import lombok.Data;
+
+@Data
+public class Ad {
+    private int adId;
+    private String image;
+    private int pk;
+    private int price;
+    private String title;
+
+}
