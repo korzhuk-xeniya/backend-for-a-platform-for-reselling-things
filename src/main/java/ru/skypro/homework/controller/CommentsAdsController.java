17861456c12904bfc23a10d4e49ded38f8ef9dea
Ксения <korzhuk.xeniya@yandex.ru>
2024-03-09 13:54:13 +0300

    Demo CommentsAdsController

--- src/main/java/ru/skypro/homework/controller/CommentsAdsController.java
+++ src/main/java/ru/skypro/homework/controller/CommentsAdsController.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.skypro.homework.dto.CommentDto;
 import ru.skypro.homework.service.AuthService;
 
 @Slf4j
@@ -26,26 +27,27 @@ public class CommentsAdsController {
                             description = "OK",
                             content = @Content(
                                     mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = Comment.class)
+                                    schema = @Schema(implementation = CommentDto.class)
                             )),
                     @ApiResponse(responseCode = "400",
                             description = "Unauthorized",
                             content = @Content(
                                     mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = Comment.class)
+                                    schema = @Schema(implementation = CommentDto.class)
 
                             )),
                     @ApiResponse(responseCode = "404",
                             description = "Not found",
                             content = @Content(
                                     mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = Comment.class)
+                                    schema = @Schema(implementation = CommentDto.class)
 
                             ))})
 
     @GetMapping("/ads/{id}/comments")
-    public ResponseEntity<Comment> getComments(@Parameter(description = "ads id") @PathVariable int id) {
-        return ResponseEntity.ok(this.adsService.findById(id));
+    public ResponseEntity<CommentDto> getComments(@Parameter(description = "ads id") @PathVariable int id) {
+//        return ResponseEntity.ok(this.adsService.findById(id));TODO
+        return ResponseEntity.ok(new CommentDto());
     }
     @Operation(summary = "Добавление комментария к объявлению",
             responses = {
@@ -53,25 +55,26 @@ public class CommentsAdsController {
                             description = "OK",
                             content = @Content(
                                     mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = Comment.class)
+                                    schema = @Schema(implementation = CommentDto.class)
                             )),
                     @ApiResponse(responseCode = "400",
                             description = "Unauthorized",
                             content = @Content(
                                     mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = Comment.class)
+                                    schema = @Schema(implementation = CommentDto.class)
 
                             )),
                     @ApiResponse(responseCode = "404",
                             description = "Not found",
                             content = @Content(
                                     mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = Comment.class)
+                                    schema = @Schema(implementation = CommentDto.class)
 
                             ))})
     @PostMapping("/ads/{id}/comments")
-    public Comment addComment(@RequestParam int id, @RequestBody String text) {
-        return commentsService.addCommentToAds( id, text);
+    public CommentDto addComment(@RequestParam int id, @RequestBody String text) {
+//        return commentsService.addCommentToAds( id, text);TODO
+        return new CommentDto();
     }
     @Operation(summary = "Удаление комментария",
             responses = {
@@ -79,32 +82,33 @@ public class CommentsAdsController {
                             description = "OK",
                             content = @Content(
                                     mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = Comment.class)
+                                    schema = @Schema(implementation = CommentDto.class)
                             )),
                     @ApiResponse(responseCode = "401",
                             description = "Unauthorized",
                             content = @Content(
                                     mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = Comment.class)
+                                    schema = @Schema(implementation = CommentDto.class)
 
                             )),
                     @ApiResponse(responseCode = "403",
                             description = "Forbidden",
                             content = @Content(
                                     mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = Comment.class)
+                                    schema = @Schema(implementation = CommentDto.class)
 
                             )),
                     @ApiResponse(responseCode = "404",
                             description = "Not found",
                             content = @Content(
                                     mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = Comment.class)
+                                    schema = @Schema(implementation = CommentDto.class)
 
                             ))})
     @DeleteMapping("/ads/{adId}/comments/{commentId}")
-    public void deleteComment(@PathVariable int id, @PathVariable int pk) {
-        commentsService.deleteComment(id,pk);
+    public void deleteComment(@PathVariable int adId, @PathVariable int commentId) {
+//        commentsService.deleteComment(adId,commentId);
+
     }
     @Operation(summary = "Обновление комментария",
             responses = {
@@ -112,31 +116,32 @@ public class CommentsAdsController {
                             description = "OK",
                             content = @Content(
                                     mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = Comment.class)
+                                    schema = @Schema(implementation = CommentDto.class)
                             )),
                     @ApiResponse(responseCode = "401",
                             description = "Unauthorized",
                             content = @Content(
                                     mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = Comment.class)
+                                    schema = @Schema(implementation = CommentDto.class)
 
                             )),
                     @ApiResponse(responseCode = "403",
                             description = "Forbidden",
                             content = @Content(
                                     mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = Comment.class)
+                                    schema = @Schema(implementation = CommentDto.class)
 
                             )),
                     @ApiResponse(responseCode = "404",
                             description = "Not found",
                             content = @Content(
                                     mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = Comment.class)
+                                    schema = @Schema(implementation = CommentDto.class)
 
                             ))})
     @PatchMapping("/ads/{adId}/comments/{commentId}")
-    public Comment updateComment(@PathVariable int id, @PathVariable int pk, @RequestBody String text) {
-        return commentsService.update(id, pk, text);
+    public CommentDto updateComment(@PathVariable int adId, @PathVariable int commentId, @RequestBody String text) {
+//        return commentsService.update(adId, commentId, text);TODO
+        return new CommentDto();
     }
 }
