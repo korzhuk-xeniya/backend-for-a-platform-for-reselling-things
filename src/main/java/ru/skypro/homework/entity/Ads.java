bcff3a069fe0512946804c5745a5f394b500ac45
Lebedeva-Elena <lebedeva_leb.en@mail.ru>
2024-03-12 23:36:15 +0300

    добавила сущность объявлений.доведу до ума связи, как будут другие таблички наглядно. пока горит красным

---
+++ src/main/java/ru/skypro/homework/entity/Ads.java
@@ -0,0 +1,50 @@
+package ru.skypro.homework.entity;
+
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Table(name = "ADS")
+public class Ads {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Integer id;
+    @Column(name = "PRICE")
+    private Integer price;
+    @Column(name = "TITLE")
+    private String title;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id") //TODO или author_id ?
+    private User author;
+    @OneToMany(mappedBy = "???")
+    @ToString.Exclude
+    private List<Image> IMAGES;
+    @OneToMany(mappedBy = "???", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    @ToString.Exclude
+    private List<Comment> comments;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Ads ads = (Ads) o;
+        return getId() != null && Objects.equals(getId(), ads.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
