11508710e75d4e9053e2576b6baa8d479af67495
Ксения <korzhuk.xeniya@yandex.ru>
2024-03-13 15:03:34 +0300

    Подключена база данных для Image and Comment

--- src/main/java/ru/skypro/homework/entity/Comment.java
+++ src/main/java/ru/skypro/homework/entity/Comment.java
@@ -7,13 +7,14 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "COMMENT")
+@Table(name = "comment")
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,12 +27,35 @@ public class Comment {
     @Column(name = "TEXT")
     private String text;
 
-    @JoinColumn(name = "ADS_ID")
-    @ManyToOne(fetch = FetchType.LAZY)
-    private Ads ads;
+//    @JoinColumn(name = "ADS_ID")
 
-    @JoinColumn(name = "USER_ID")
-    @ManyToOne(fetch = FetchType.EAGER)
-    private User user;
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    private Ads ads;
+//
+//    @JoinColumn(name = "USER_ID")
+//    @ManyToOne(fetch = FetchType.EAGER)
+//    private User user;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Comment comment = (Comment) o;
+        return Objects.equals(id, comment.id) && Objects.equals(createdAt, comment.createdAt) && Objects.equals(text, comment.text);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, createdAt, text);
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", createdAt=" + createdAt +
+                ", text='" + text + '\'' +
+                '}';
+    }
+
 
 }
