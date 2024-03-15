49a1e26c41bc1f96c061747da7fc0c6d847e3d06
glinka <alexandraglinka@yandex.ru>
2024-03-15 20:12:58 +0300

    User - ограничения дла полей

--- src/main/java/ru/skypro/homework/entity/User.java
+++ src/main/java/ru/skypro/homework/entity/User.java
@@ -1,9 +1,11 @@
 package ru.skypro.homework.entity;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 import ru.skypro.homework.dto.Role;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Entity
@@ -13,15 +15,22 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID", nullable = false)
+    @Size(max=32)
     private Integer id;
 
     @Column(name = "EMAIL", nullable = false)
+    @Length(min = 4)
+    @Length(max = 32)
     private String email;
 
     @Column(name = "FIRST_NAME", nullable = false)
+    @Length(min = 3)
+    @Length(max = 16)
     private String firstName;
 
     @Column(name = "LAST_NAME", nullable = false)
+    @Length(min = 3)
+    @Length(max = 16)
     private String lastName;
 
     @Column(name = "PHONE", nullable = false)
@@ -31,19 +40,16 @@ public class User {
     private Role role;
 
     @Column(name = "PASSWORD", nullable = false)
+    @Length(min = 8)
+    @Length(max = 16)
     private String password;
 
-//    @Column(name = "REG_DATE", nullable = false)
-//    private LocalDateTime regDate;
+    @Column(name = "REG_DATE", nullable = false)
+    private LocalDateTime regDate;
 
-//    @OneToOne
-//    @JoinColumn(name = "id")
-//    @Column(name = "AVATAR", nullable = false)
-//    private Image avatar;
-
-//    @OneToMany
-//    @JoinColumn(name = "ID")
-//    @Column(name = "ADS", nullable = false)
-//    private List<> ads;
+    @OneToOne
+    @JoinColumn(name = "id")
+    @Column(name = "AVATAR", nullable = true)
+    private Image avatar;
 
 }
