3e96716bac17975117d80349ffb4112a3907903c
glinka <alexandraglinka@yandex.ru>
2024-03-15 20:37:10 +0300

    UserDto, UpdateUserDto - ограничения дла полей

--- src/main/java/ru/skypro/homework/dto/UpdateUserDto.java
+++ src/main/java/ru/skypro/homework/dto/UpdateUserDto.java
@@ -1,14 +1,25 @@
 package ru.skypro.homework.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.stereotype.Service;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Service
 public class UpdateUserDto {
 
+    @Schema(description = "имя пользователя")
+    @Size(min = 3, max = 10)
     private String firstName;
 
+    @Schema(description = "фамилия пользователя")
+    @Size(min = 3, max = 10)
     private String lastName;
 
+    @Schema(description = "телефон пользователя")
+    @Pattern(regexp = "\\+7\\s?\\(?\\d{3}\\)?\\s?\\d{3}-?\\d{2}-?\\d{2}")
     private String phone;
 
     public UpdateUserDto firstName(String firstName) {
--- src/main/java/ru/skypro/homework/dto/UserDto.java
+++ src/main/java/ru/skypro/homework/dto/UserDto.java
@@ -1,21 +1,33 @@
 package ru.skypro.homework.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import org.springframework.jmx.export.annotation.ManagedAttribute;
+
+import javax.validation.constraints.Max;
 
 @Data
 public class UserDto {
 
+    @Schema(description = "id пользователя")
+    @Max(32)
     private Integer id;
 
+    @Schema(description = "логин пользователя")
     private String email;
 
+    @Schema(description = "имя пользователя")
     private String firstName;
 
+    @Schema(description = "фамилия пользователя")
     private String lastName;
 
+    @Schema(description = "телефон пользователя")
     private String phone;
 
+    @Schema(description = "роль пользователя")
     private Role role;
 
+    @Schema(description = "ссылка на аватар пользователя")
     private String image;
 }
