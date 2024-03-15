834e110706c69598312e48f14143c2eb415b364a
Kseniia313 <kshome@mail.ru>
2024-03-15 20:32:20 +0300

    Добавлен mapStruct конфигурация

---
+++ src/main/java/ru/skypro/homework/entity/Comment.java
@@ -0,0 +1,61 @@
+package ru.skypro.homework.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "comment")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false)
+    private Integer id;
+
+    @Column(name = "CREATED_AT")
+    private LocalDateTime createdAt;
+
+    @Column(name = "TEXT")
+    private String text;
+
+//    @JoinColumn(name = "ADS_ID")
+
+    //    @ManyToOne(fetch = FetchType.LAZY)
+//    private Ads ads;
+//
+//    @JoinColumn(name = "USER_ID")
+//    @ManyToOne(fetch = FetchType.EAGER)
+//    private User user;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Comment comment = (Comment) o;
+        return Objects.equals(id, comment.id) && Objects.equals(createdAt, comment.createdAt) && Objects.equals(text, comment.text);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, createdAt, text);
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", createdAt=" + createdAt +
+                ", text='" + text + '\'' +
+                '}';
+    }
+
+
+}
