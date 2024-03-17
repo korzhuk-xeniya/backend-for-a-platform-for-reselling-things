f6772f944a5007a9e6b812c7fa2c0b95fb4dc80c
Ксения <korzhuk.xeniya@yandex.ru>
2024-03-17 18:20:48 +0300

    Исправила на Integer поле createdAt

--- src/main/java/ru/skypro/homework/dto/CommentDto.java
+++ src/main/java/ru/skypro/homework/dto/CommentDto.java
@@ -7,7 +7,7 @@ public class CommentDto {
     private Integer author;
     private String authorImage;
     private String authorFirstName;
-    private Long createdAt;
+    private Integer createdAt;
     private Integer pk;
     private String text;
 }
