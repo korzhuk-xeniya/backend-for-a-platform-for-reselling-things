8e2ec54b4a9581154cdaef465eb2d0667f697703
glinka <alexandraglinka@yandex.ru>
2024-03-21 19:48:43 +0300

    добавлен тестовый пользователь в users.sql

--- src/main/resources/liquibase/scripts/users.sql
+++ src/main/resources/liquibase/scripts/users.sql
@@ -13,3 +13,8 @@ CREATE TABLE users
     REG_DATE    TIMESTAMP NOT NULL,
     AVATAR_ID   INTEGER references image (id) on delete set null
 );
+
+------changeset aglinka:insert into users
+INSERT INTO users
+(ID, EMAIL, FIRST_NAME, LAST_NAME, PHONE, ROLE, PASSWORD, REG_DATE)
+VALUES (1, 'user@gmail.com', 'test', 'test', '+79998887766', 'USER', 'password', CURRENT_TIMESTAMP)
