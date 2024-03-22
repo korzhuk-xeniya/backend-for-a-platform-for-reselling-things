37d3777b9aad4ed9f6b77097d7e21e5ca8464c34
glinka <alexandraglinka@yandex.ru>
2024-03-22 23:40:18 +0300

    исправлен маппер и тест для пользователей

--- src/main/java/ru/skypro/homework/repository/AdsRepository.java
+++ src/main/java/ru/skypro/homework/repository/AdsRepository.java
@@ -2,26 +2,13 @@ package ru.skypro.homework.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.Query;
 import ru.skypro.homework.entity.Ads;
 import ru.skypro.homework.entity.Comment;
 import ru.skypro.homework.entity.User;
-
-import java.util.Collection;
 
 import java.util.List;
 
 @Repository
 public interface AdsRepository extends JpaRepository<Ads, Integer> {
     List<Ads> findAllByUser (User user);
-
-    /**
-     * метод возвращает все объявления при частичном совпадении названия или описания объявления
-     */
-    @Query(value = "SELECT a.* " +
-            "FROM ads a " +
-            "WHERE title LIKE '%?1%' " +
-            "OR description LIKE '%?1%'", nativeQuery = true)
-    Collection<Ads> findByAdsNameOrDescriptionIgnoreCase(String text);
-
 }
