06979a3e22c5551506610d0fde16a6226957cb32
glinka <alexandraglinka@yandex.ru>
2024-03-24 20:22:18 +0300

    UserServiceImplTest / updateAuthUserInfoTest

--- src/test/java/ru/skypro/homework/service/impl/UserServiceImplTest.java
+++ src/test/java/ru/skypro/homework/service/impl/UserServiceImplTest.java
@@ -12,8 +12,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.skypro.homework.dto.Role;
+import ru.skypro.homework.dto.UpdateUserDto;
 import ru.skypro.homework.entity.Image;
 import ru.skypro.homework.entity.User;
+import ru.skypro.homework.mapper.UserMapper;
 import ru.skypro.homework.repository.UserRepository;
 
 import java.util.Optional;
@@ -22,6 +24,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -30,15 +33,20 @@ public class UserServiceImplTest {
     private UserRepository userRepository;
     @Mock
     private Authentication authentication;
+    @Mock
+    private UserMapper userMapper;
     @InjectMocks
     private UserServiceImpl userService;
 
+    @Test
+    public void setPasswordTest() {
 
+    }
     @Test
     public void getAuthUserInfoTest() {
 
         when(authentication.getName()).thenReturn(testUser().getEmail());
-        when(userRepository.findUserByEmail(anyString())).thenReturn(Optional.ofNullable(testUser()));
+        when(userRepository.findUserByEmail(authentication.getName())).thenReturn(Optional.ofNullable(testUser()));
 
         User result = userService.getAuthUserInfo(authentication);
 
@@ -57,6 +65,41 @@ public class UserServiceImplTest {
         assertThat(result.getAvatar().getMediaType()).isEqualTo(testUser().getAvatar().getMediaType());
 
     }
+
+    @Test
+    public void updateAuthUserInfoTest() {
+        User user = testUser();
+        UpdateUserDto updateUserDto = updateUserDto();
+
+        given(userRepository.findUserByEmail(anyString())).willReturn(Optional.of(user));
+
+        User updatedUser = user;
+        user.setFirstName(updateUserDto.getFirstName());
+        user.setLastName(updateUserDto.getLastName());
+        user.setPhone(updateUserDto.getPhone());
+
+        given(userRepository.save(any(User.class))).willReturn(updatedUser);
+        given(authentication.getName()).willReturn(user.getEmail());
+        given(userMapper.userToUpdateUserDto(any(User.class))).willReturn(updateUserDto);
+
+        UpdateUserDto result = userService.updateAuthUserInfo(updateUserDto, authentication);
+
+        assertThat(result).isNotNull();
+        assertThat(result.getFirstName()).isEqualTo(updateUserDto.getFirstName());
+        assertThat(result.getLastName()).isEqualTo(updateUserDto.getLastName());
+        assertThat(result.getPhone()).isEqualTo(updateUserDto.getPhone());
+
+        assertThat(user.getFirstName()).isEqualTo(updateUserDto().getFirstName());
+        assertThat(user.getLastName()).isEqualTo(updateUserDto().getLastName());
+        assertThat(user.getPhone()).isEqualTo(updateUserDto().getPhone());
+        assertThat(user.getId()).isEqualTo(testUser().getId());
+    }
+
+    @Test
+    public void updateAvatarTest() {
+
+    }
+
 
     private User testUser() {
 
@@ -92,5 +135,16 @@ public class UserServiceImplTest {
         Authentication auth = new UsernamePasswordAuthenticationToken(testUser(), null);
 
         return auth;
+    }
+
+    private UpdateUserDto updateUserDto() {
+
+        UpdateUserDto updateUserDto = new UpdateUserDto();
+
+        updateUserDto.setFirstName("new firstname");
+        updateUserDto.setLastName("new lastname");
+        updateUserDto.setPhone("+79990009988");
+
+        return updateUserDto;
     }
 }
